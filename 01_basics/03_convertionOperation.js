8991a0a6b1be55a94a084542c5ddb07b63ffc850
Deepanshu Dutta <72452775+deepanshudutta743@users.noreply.github.com>
2024-06-09 11:38:56 +0000

    added aperitions

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -1,10 +1,10 @@
 let score=null
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber=Number(score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
 
 // "33"->33
 // "33abc"-> NaN
@@ -12,7 +12,7 @@ console.log(typeof valueInNumber);
 
 let isLogedin="asc"
 let booleanIsLogedIn=Boolean(isLogedin);
-console.log(booleanIsLogedIn);
+//console.log(booleanIsLogedIn);
 
 // 1 -> true; 0->false
 // "" -> false
@@ -20,5 +20,30 @@ console.log(booleanIsLogedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ****************** Operiations *********************
+
+let value=3
+let negValue=-value;
+console.log(negValue);
+
+console.log(2**3);
+
+let str1="hello";
+let str2=" Deeapnshu";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2+2)
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+
+
+
+
