de94556fa2383d7676052e6dce145716a7c9c51c
Deepanshu Dutta <72452775+deepanshudutta743@users.noreply.github.com>
2024-06-09 11:15:26 +0000

    convertion of DataTypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,24 @@
+let score=null
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33"->33
+// "33abc"-> NaN
+// true->1 false ->0
+
+let isLogedin="asc"
+let booleanIsLogedIn=Boolean(isLogedin);
+console.log(booleanIsLogedIn);
+
+// 1 -> true; 0->false
+// "" -> false
+// "abc" -> true 
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
